174f00cb018f35789adbc6ae5f679dd4b7bb04b1
Martin Slemr <mslemr@redhat.com>
2019-04-26 13:20:37 +0200

    Full refresh inifinite loop

--- lib/topological_inventory/mock_source/collector.rb
+++ lib/topological_inventory/mock_source/collector.rb
@@ -20,10 +20,34 @@ module TopologicalInventory
       end
 
       def collect!
-        if ::Settings.multithreading == :on
+        loop do
+          #
+          # Collect each entity type in 1 / separate threads
+          #
+          if ::Settings.multithreading == :on
+            collect_in_threads!
+          else
+            collect_sequential!
+          end
+
+          if ::Settings.refresh_mode == :full_refresh
+            sleep_duration = ::Settings.full_refresh&.sleep_duration.to_i
+            logger.info("Full refresh finished, sleeping #{sleep_duration} seconds...")
+            sleep(sleep_duration)
+          else
+            break
+          end
+        end
+      end
+
+      def collect_in_threads!
+        if %i[standard events].include?(::Settings.refresh_mode)
           super
-        else
-          collect_sequential!
+        else # full_refresh
+          start_collector_threads
+
+          # wait for all threads
+          sleep(poll_time) until finished?
         end
       end
 
@@ -49,10 +73,6 @@ module TopologicalInventory
         end
       rescue => err
         logger.error(err)
-      end
-
-      def stop
-        finished.value = true
       end
 
       protected
@@ -121,40 +141,38 @@ module TopologicalInventory
       end
 
       def full_refresh(entity_type)
-        (::Settings.full_refresh&.repeats_count || 1).to_i.times do |cnt|
-          resource_version = continue = nil
+        resource_version = continue = nil
 
-          refresh_state_uuid = SecureRandom.uuid
-          logger.info("[#{cnt}] Collecting #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...")
+        refresh_state_uuid = SecureRandom.uuid
+        logger.info("Collecting #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...")
 
-          total_parts = 0
-          loop do
-            entities = connection.send("get_#{entity_type}", :limit => limits[entity_type], :continue => continue)
-            break if entities.nil?
+        total_parts = 0
+        loop do
+          entities = connection.send("get_#{entity_type}", :limit => limits[entity_type], :continue => continue)
+          break if entities.nil?
 
-            continue         = entities.continue
-            resource_version = entities.resource_version
+          continue         = entities.continue
+          resource_version = entities.resource_version
 
-            parser = parser_class.new
-            parser.parse_entities(entity_type, entities, storage_class.entity_types[entity_type])
-            refresh_state_part_uuid = SecureRandom.uuid
-            total_parts += 1
-            save_inventory(parser.collections.values, refresh_state_uuid, refresh_state_part_uuid)
+          parser = parser_class.new
+          parser.parse_entities(entity_type, entities, storage_class.entity_types[entity_type])
+          refresh_state_part_uuid = SecureRandom.uuid
+          total_parts             += 1
+          save_inventory(parser.collections.values, refresh_state_uuid, refresh_state_part_uuid)
 
-            break if entities.last?
-          end
-
-          logger.info("[#{cnt}] Collecting #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...Complete - Parts [#{total_parts}]")
-
-          full_refresh_sweep(cnt, entity_type, refresh_state_uuid, resource_version, total_parts)
+          break if entities.last?
         end
+
+        logger.info("Collecting #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...Complete - Parts [#{total_parts}]")
+
+        full_refresh_sweep(entity_type, refresh_state_uuid, resource_version, total_parts)
       rescue => e
         logger.error("Error collecting :#{entity_type}, message => #{e.message}")
         raise e
       end
 
-      def full_refresh_sweep(cnt, entity_type, refresh_state_uuid, resource_version, total_parts)
-        logger.info("[#{cnt}] Sweeping inactive records for #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...")
+      def full_refresh_sweep(entity_type, refresh_state_uuid, resource_version, total_parts)
+        logger.info("Sweeping inactive records for #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...")
 
         parsed_entity_types = [entity_type] + (storage_class.entity_types[entity_type] || []).flatten.compact
         # parsed_entity_types = [entity_type]
@@ -163,7 +181,7 @@ module TopologicalInventory
                         total_parts,
                         parsed_entity_types)
 
-        logger.info("[#{cnt}] Sweeping inactive records for #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...Complete")
+        logger.info("Sweeping inactive records for #{entity_type} with :refresh_state_uuid => '#{refresh_state_uuid}'...Complete")
         resource_version
       end
 
